ad65aec3d654bd6baa9b421788a0d136e7c3835e
phr3nzy <adilosama47@gmail.com>
2019-01-22 16:35:53 +0200

    Added null token protection. must relog before submitting items!

--- static/sw.js
+++ static/sw.js
@@ -2,76 +2,76 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0329cda9f5c1a6bb6969.js",
-    "revision": "7d04825675fe9225b6299da3a6f668f6"
+    "url": "/_nuxt/0a498f1d10a717088813.js",
+    "revision": "4b7df96ebb08bd71e51a2ff5a56a704e"
   },
   {
-    "url": "/_nuxt/04445cb7a968a25b10e9.js",
-    "revision": "7ef008ea8eca77a79176bfc767325ecc"
+    "url": "/_nuxt/0aa085b4486a8ffdfbeb.js",
+    "revision": "4c561e05e9b4840bc791c83dfc125499"
   },
   {
-    "url": "/_nuxt/0a5ded48fbfa2ec99ded.js",
-    "revision": "236c8c405791d2685e7a39699784ade3"
+    "url": "/_nuxt/1b211e29f7a21365a5f8.js",
+    "revision": "9a76f8a7a2e802bf0daa121e430bd27a"
   },
   {
-    "url": "/_nuxt/1cb6bd25b81625cebef3.js",
-    "revision": "64c084a241f336d3d5b8c726650302e1"
+    "url": "/_nuxt/276446ef09e7a9ea0b46.js",
+    "revision": "03ded7ed290939362157a7693224f3f0"
   },
   {
-    "url": "/_nuxt/27215015386581b1084a.js",
-    "revision": "7ebaac0256c0240b113fb49fa1a4c5a5"
+    "url": "/_nuxt/33540990055ef71de38a.js",
+    "revision": "bac182904fe2810422a4a018c01f2344"
   },
   {
-    "url": "/_nuxt/3571d63d9a2e544da7f2.js",
-    "revision": "1ac4e659c12eb8006bf20f410e3b44d9"
+    "url": "/_nuxt/457ac1cb572f84e95454.js",
+    "revision": "0c549f4d60213fff23ec8bb22e29ad6b"
   },
   {
-    "url": "/_nuxt/4af6ec9e10a172954d37.js",
-    "revision": "823a22b467a7b07595360bf0960f6f93"
+    "url": "/_nuxt/48eb58bbb01cbf0e7ef9.js",
+    "revision": "2c2bd44a416acc289675644b650bdd3c"
   },
   {
-    "url": "/_nuxt/61316c2fbd38124b5020.js",
-    "revision": "79a4dd8bc10550720dc5ee7ba2e50534"
+    "url": "/_nuxt/65affc148dac7fe4c4f5.js",
+    "revision": "507a4548d98fd1de8eb127ce9ddc1940"
   },
   {
-    "url": "/_nuxt/644af0acee075496dbc8.js",
-    "revision": "3f00f8d48339b599223d35e6abd30396"
+    "url": "/_nuxt/6de2322e43cd6b067261.js",
+    "revision": "949454a0d4f2c028cdfcde7c53f36083"
   },
   {
-    "url": "/_nuxt/8bdd93b83542fa18f6ff.js",
-    "revision": "b632a81712fa18904f221dcafb7b9e73"
+    "url": "/_nuxt/6ea8dc8e030bca138599.js",
+    "revision": "92eab3f13710ca06f379ccbd760851de"
   },
   {
-    "url": "/_nuxt/9646e42b9378a8bec675.js",
-    "revision": "a092c1664e80e8fa6b713d718a67a8a2"
+    "url": "/_nuxt/95cb95c2c7054b27e432.js",
+    "revision": "c8aa3f06df08cef7482180e81d130871"
   },
   {
-    "url": "/_nuxt/9b214d2f80f8fcd729d2.js",
-    "revision": "94da6bbed5568b5e736fe7b18eee81c0"
+    "url": "/_nuxt/a1568b489fd67054359d.js",
+    "revision": "f6d6a9485d9c26bab4ce2d7e0ab442e0"
   },
   {
-    "url": "/_nuxt/9eb07820a9772cd7b68b.js",
-    "revision": "cfa51936cdf8d4f1af14c5d246c5bda7"
+    "url": "/_nuxt/aaf23d83e0ea932fa8b9.js",
+    "revision": "f9315ff9dcc1242fd3ed21a537791fd4"
   },
   {
-    "url": "/_nuxt/a75f97a7961a87fb20d1.js",
-    "revision": "b4c202a6809a34eabc28930af9bc2865"
+    "url": "/_nuxt/b6cb9704339fa8e9c149.js",
+    "revision": "ebf50f2c4bbcaf74ac70371bfc014ae3"
   },
   {
-    "url": "/_nuxt/c3ade1360cf41d2b89e2.js",
-    "revision": "ead9c11e92cf045a6955de1087d8ebfa"
+    "url": "/_nuxt/bb8090677e4276966314.js",
+    "revision": "9c9356bf853a1dd0076654e55991ce45"
   },
   {
-    "url": "/_nuxt/cc9d1e5eb84fc3599668.js",
-    "revision": "7ad3a2704b919c77b6d7cf592d651b7f"
+    "url": "/_nuxt/c3476d63aa9a3da130d1.js",
+    "revision": "764f76be63cb2b42c333369b6aa6bb81"
   },
   {
-    "url": "/_nuxt/dc8848a5db007e73d349.js",
-    "revision": "d6caa22ba32f79d992b9a185594ea5c0"
+    "url": "/_nuxt/c42a61a7cbc357d214f7.js",
+    "revision": "e87d6aa61296284a7467cf2bd6b4f5a9"
   },
   {
-    "url": "/_nuxt/e007c54ac8d0f9dd23d2.js",
-    "revision": "07856c3c8a4ef745f410c8769244f647"
+    "url": "/_nuxt/dd81da6f65937b1ae56b.js",
+    "revision": "d24986dbd1e2eabb0772d7067477a509"
   }
 ], {
   "cacheId": "Al-Dokkaan",
